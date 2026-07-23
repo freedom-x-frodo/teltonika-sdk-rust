2b12bbc9286cd9f0868328b84731353ccd3733ae
rassulya <rasul.yermagambet@gmail.com>
2026-07-23 13:05:45 +0500

    feat: add device struct to get validate device type and ip

---
+++ teltonika-core/src/device.rs
@@ -0,0 +1,74 @@
+use auth::Credentials;
+use std::collections::HashMap;
+
+struct DeviceType {
+    pub model: String,
+    pub firmware_version: String,
+}
+
+impl DeviceType {
+    pub fn new(model: String, firmware_version: String) -> Self {
+        DeviceType {
+            model,
+            firmware_version,
+        }
+    }
+    fn available_device_types() -> HashMap<String, String> {
+        todo!("Implement available device types for further validation. Get device types from database or configuration file.");
+        let device_types = HashMap::new();
+        device_types.insert("RUTX50".to_string(), "RUTX_R_00.07.23.7".to_string());
+        device_types
+    }
+    fn validate_device_type(model: &str, firmware_version: &str) -> bool {
+        let device_types = DeviceType::available_device_types();
+        if let Some(expected_firmware) = device_types.get(model) {
+            return expected_firmware == firmware_version;
+        }
+        false
+    }
+    
+}
+
+pub struct Device {
+    pub device_type: DeviceType,
+    pub ip: String,
+}
+
+impl Device {
+    pub fn new(model: String, firmware_version: String, ip: String) -> Self {
+        if !DeviceType::validate_device_type(&model, &firmware_version) {
+            panic!("Invalid device type: model '{}' with firmware version '{}'.
+                    Available device types are: {:?}", 
+                    model, firmware_version, DeviceType::available_device_types());
+        }
+        if !Device::validate_ip(&ip) {
+            panic!("Invalid IP address: '{}'. Please provide a valid IPv4 address.", ip);
+        }
+        Device {
+            device_type: DeviceType {
+                model,
+                firmware_version,
+            },
+            ip,
+        }
+    }
+    fn validate_ip(ip: &str) -> bool {
+        // Simple IP address validation (IPv4)
+        let segments: Vec<&str> = ip.split('.').collect();
+        if segments.len() != 4 {
+            return false;
+        }
+        for segment in segments {
+            if let Ok(num) = segment.parse::<u8>() {
+                if num > 255 {
+                    return false;
+                }
+            } else {
+                return false;
+            }
+        }
+        true
+    }
+}
+
+
