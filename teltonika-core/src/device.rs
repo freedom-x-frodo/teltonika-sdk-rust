c6cc876c97a5d501c2a565ee1dd8f61f3299470b
rassulya <rasul.yermagambet@gmail.com>
2026-07-23 18:44:53 +0500

    clippy cleanup

--- teltonika-core/src/device.rs
+++ teltonika-core/src/device.rs
@@ -1,23 +1,27 @@
 use std::collections::HashMap;
-use serde::Serialize;
+
+#[allow(dead_code)]
 struct DeviceType {
     pub model: String,
     pub firmware_version: String,
 }
 
 impl DeviceType {
+    #[allow(dead_code)]
     pub fn new(model: String, firmware_version: String) -> Self {
         DeviceType {
             model,
             firmware_version,
         }
     }
+    #[allow(dead_code)]
     fn available_device_types() -> HashMap<String, String> {
-        todo!("Implement available device types for further validation. Get device types from database or configuration file.");
-        let device_types = HashMap::new();
+        //TODO: Implement available device types for further validation. Get device types from database or configuration file
+        let mut device_types = HashMap::new();
         device_types.insert("RUTX50".to_string(), "RUTX_R_00.07.23.7".to_string());
         device_types
     }
+    #[allow(dead_code)]
     fn validate_device_type(model: &str, firmware_version: &str) -> bool {
         let device_types = DeviceType::available_device_types();
         if let Some(expected_firmware) = device_types.get(model) {
