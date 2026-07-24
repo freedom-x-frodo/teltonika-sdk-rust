d3af6da2f5952e7019d753454f2cf978307b188b
rassulya <rasul.yermagambet@gmail.com>
2026-07-24 12:44:17 +0500

    refactor: remove redundant example functoins from lib files of crates

--- teltonika-core/src/lib.rs
+++ teltonika-core/src/lib.rs
@@ -4,17 +4,3 @@ pub mod error;
 
 pub use error::{Result, TeltonikaError};
 
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
--- teltonika-rest/src/lib.rs
+++ teltonika-rest/src/lib.rs
@@ -1,20 +1,6 @@
 pub mod client;
-mod auth;
+pub mod auth;
 mod utils;
 mod error;
 pub mod api;
 
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
