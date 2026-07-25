827996a64fc1cde12e9c0fad40f2956443bc43cb
rassulya <rasul.yermagambet@gmail.com>
2026-07-25 12:47:59 +0500

    feat: add example to get modems data for smoke test on the robot dog

---
+++ teltonika-rest/examples/modems.rs
@@ -0,0 +1,27 @@
+//! Reads /modems/status from a live RUTX50.
+//!
+//! RUTX_HOST=192.168.123.1 RUTX_USER=admin RUTX_PASS=... cargo run --example modems
+
+use std::env;
+
+use teltonika_core::config::ConnConfig;
+use teltonika_rest::auth::AuthType;
+use teltonika_rest::client::RestClient;
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let host = env::var("RUTX_HOST")?;
+    let user = env::var("RUTX_USER")?;
+    let pass = env::var("RUTX_PASS")?;
+
+    let config = ConnConfig::new(user, pass, host);
+    let client = RestClient::connect(config, AuthType::Session).await?;
+
+    let modems = client.modems().status().await?;
+    println!("{} modem(s)\n", modems.len());
+    for modem in &modems {
+        println!("{modem:#?}\n");
+    }
+
+    Ok(())
+}
