2dae44affe628edf1e9945e6b0037bb4b920a2ed
rassulya <rasul.yermagambet@gmail.com>
2026-07-28 12:59:13 +0500

    feat: add refresh margin to connconfig for session token expiration

--- teltonika-core/src/config.rs
+++ teltonika-core/src/config.rs
@@ -1,16 +1,26 @@
-pub struct ConnConfig{
-    pub username: String, 
+use std::time::Duration;
+
+pub struct ConnConfig {
+    pub username: String,
     pub password: String,
     pub ip: String,
-    pub accept_invalid_certs: bool
+    pub accept_invalid_certs: bool,
+    pub refresh_margin: Duration, //expiration time renewal for session login
 }
+
 impl ConnConfig {
     pub fn new(username: String, password: String, ip: String) -> Self {
         Self {
             username,
             password,
             ip,
-            accept_invalid_certs: true
+            accept_invalid_certs: true,
+            refresh_margin: Duration::from_secs(30),
         }
+    }
+
+    pub fn with_refresh_margin(mut self, refresh_margin: Duration) -> Self {
+        self.refresh_margin = refresh_margin;
+        self
     }
 }
